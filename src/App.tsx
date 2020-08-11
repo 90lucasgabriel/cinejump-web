4675b7ac39297f8c7ea69c2b97fb45f62cea6f2b
Lucas Teixeira <lucas.teixeira@webjump.com.br>
2020-08-11 00:19:01 -0300

    WJ-0 - New: First settings;

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import { BrowserRouter as Router } from 'react-router-dom';
 
-function App() {
+// import AppProvider from './hooks';
+// import Routes from './routes';
+import { Layout } from 'components/Layout';
+import GlobalStyle from './styles/global';
+
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Layout>
+        <h1>Hello World</h1>
+      </Layout>
+
+      <GlobalStyle />
+    </>
   );
-}
+};
 
 export default App;
---
+++ src/components/Layout/index.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const Layout = styled.div`
+  height: 100%;
+`;
+
+export const Wrapper = styled.div`
+  max-width: 1280px;
+  margin: 0 auto;
+`;
