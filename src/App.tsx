64d382a121a4d5b6e8cad8a68243d88a31ee1d18
Lucas Teixeira <lucas.teixeira@webjump.com.br>
2020-08-15 21:53:36 -0300

    WJ-0 - New: Basic Home architecture;

--- src/App.tsx
+++ src/App.tsx
@@ -3,25 +3,13 @@ import React from 'react';
 
 // import AppProvider from './hooks';
 // import Routes from './routes';
-import { ColumnLayout, Wrapper, Container } from 'components/Layout';
+import Home from 'screens/Home';
 import GlobalStyle from './styles/global';
 
 const App: React.FC = () => {
   return (
     <>
-      <ColumnLayout>
-        <Wrapper>
-          <Container>
-            <h1>Hello World</h1>
-          </Container>
-        </Wrapper>
-        <Wrapper>
-          <Container>
-            <h1>Hello World</h1>
-          </Container>
-        </Wrapper>
-
-      </ColumnLayout>
+      <Home />
 
       <GlobalStyle />
     </>
