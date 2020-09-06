0930ec2cfc1602911a5e01fa6db9dc2c32b746bb
Lucas Teixeira <lucas.teixeira@webjump.com.br>
2020-09-06 14:15:54 -0300

    WJ-41 - New: Check user when update favorite;

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 
+import { useAuth } from 'domains/Auth/hooks';
 import { useFavorite } from 'domains/Favorites/hooks';
 import { NowPlaying, Popular } from 'domains/Movie/api';
 import MovieResponse from 'domains/Movie/api/Popular/Response';
@@ -12,13 +13,17 @@ const Home: React.FC = () => {
   const [popularList, setPopularList] = useState([] as MovieResponse[]);
   const [nowPlayingList, setNowPlayingList] = useState([] as MovieResponse[]);
 
+  const { user } = useAuth();
   const { Favorites, favoriteList } = useFavorite();
 
   useEffect(() => {
     NowPlaying().then(response => setNowPlayingList(response));
     Popular().then(response => setPopularList(response));
-    Favorites();
-  }, [Favorites]);
+
+    if (user) {
+      Favorites();
+    }
+  }, [user, Favorites]);
 
   return (
     <ColumnLayout>
@@ -27,7 +32,10 @@ const Home: React.FC = () => {
         <Highlights movies={popularList} />
         <MovieList theme="light" title="Populares" data={popularList} />
         <MovieList theme="light" title="Lançamentos" data={nowPlayingList} />
-        <MovieList theme="light" title="Favoritos" data={favoriteList} />
+
+        {user && (
+          <MovieList theme="light" title="Favoritos" data={favoriteList} />
+        )}
       </ContentContainer>
       <Footer />
       <HeaderBackground />
