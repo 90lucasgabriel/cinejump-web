c7fdb91deb3394fc22c3d160487c56026cd6ec31
Lucas Teixeira <lucas.teixeira@webjump.com.br>
2020-09-19 15:12:25 -0300

    WJ-47 - New: Movie stories;

--- src/components/Movie/dtos/index.ts
+++ src/components/Movie/dtos/index.ts
@@ -1,5 +1,6 @@
-import PopularProps from 'domains/Movie/api/Popular/Response';
-
-export default interface Props extends PopularProps {
-  isFavorite?: boolean;
+export default interface Props {
+  id: number;
+  favorite: boolean;
+  poster?: string;
+  backdrop?: string;
 }
