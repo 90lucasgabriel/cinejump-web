0df8e16ec77c8280f963f27befe83beabf375fc3
Lucas Teixeira <lucas.teixeira@webjump.com.br>
2020-11-01 14:22:49 -0300

    WJ-30 - Update: Profile page layout;

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -2,8 +2,11 @@ import React, { useCallback } from 'react';
 import { useHistory } from 'react-router-dom';
 
 import { useAuth } from 'domains/Auth/hooks';
-import { Button } from 'components';
-import { Container } from './styles';
+
+import { ReactComponent as Avatar } from 'assets/avatar.svg';
+import { ColumnLayout, Button } from 'components';
+import { Footer, Header } from 'containers';
+import { ContentContainer } from './styles';
 
 const Profile: React.FC = () => {
   const history = useHistory();
@@ -15,10 +18,17 @@ const Profile: React.FC = () => {
   }, [logout, history]);
 
   return (
-    <Container>
-      <h1>Profile Screen</h1>
-      <Button onClick={handleLogout}>Logout</Button>
-    </Container>
+    <ColumnLayout>
+      <Header theme="secondary" />
+      <ContentContainer>
+        <Avatar />
+
+        <Button onClick={handleLogout} variant="outlined" theme="light">
+          Logout
+        </Button>
+      </ContentContainer>
+      <Footer theme="secondary" />
+    </ColumnLayout>
   );
 };
 
